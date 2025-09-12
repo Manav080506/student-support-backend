fca673a9de281275935771cac8ab420eddde1d7d
Manav080506 <manavrunthala11@gmail.com>
2025-09-12 22:29:53 +0530

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// models/index.js
+import Student from "./Student.js";
+import Parent from "./Parent.js";
+import Mentor from "./Mentor.js";
+import Faq from "./Faq.js";
+
+export { Student, Parent, Mentor, Faq };
