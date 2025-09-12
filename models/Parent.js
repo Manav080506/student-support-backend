10875db9e3af8898097a95493eb05d038aeefb34
Manav080506 <manavrunthala11@gmail.com>
2025-09-12 22:09:59 +0530

    Update and rename Parents.js to Parent.js

--- models/Parent.js
+++ models/Parent.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const parentSchema = new mongoose.Schema({
   parentId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  relation: String,
+  relation: String, // e.g., Father, Mother
   studentId: { type: String, required: true } // link to Student
 });
 
