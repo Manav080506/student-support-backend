3796d439a6a77a18b2b506ad9a1478024c7d2a79
Manav080506 <manavrunthala11@gmail.com>
2025-09-12 20:34:02 +0530

    Create Faq.js

---
+++ models/Faq.js
@@ -0,0 +1,12 @@
+// models/Faq.js
+import mongoose from "mongoose";
+
+const faqSchema = new mongoose.Schema({
+  category: { type: String, required: true }, // e.g., "Finance", "Counseling"
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Faq = mongoose.model("Faq", faqSchema);
+export default Faq;
