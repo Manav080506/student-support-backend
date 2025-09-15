dbd64eafafdec4cd8143e9c6cb323766f575ad44
Manav080506 <manavrunthala11@gmail.com>
2025-09-15 16:53:22 +0530

    Update Faq.js

--- models/Faq.js
+++ models/Faq.js
@@ -2,10 +2,10 @@
 import mongoose from "mongoose";
 
 const faqSchema = new mongoose.Schema({
-  category: { type: String, required: true }, // e.g. "Finance", "Counseling"
+  category: { type: String, required: true }, 
   question: { type: String, required: true },
   answer: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Faq = mongoose.models.Faq || mongoose.model("Faq", faqSchema);
