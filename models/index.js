a7200e951312d9facf328a1cf0c77e1ad9d1b07e
Manav080506 <manavrunthala11@gmail.com>
2025-09-15 16:51:34 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
-// models/index.js
 import Student from "./Student.js";
 import Parent from "./Parent.js";
 import Mentor from "./Mentor.js";
 import Faq from "./Faq.js";
+import ChatLog from "./ChatLog.js";
+import Badge from "./Badge.js";
+import BadgeMeta from "./BadgeMeta.js";
+import Reminder from "./Reminder.js";
 
-export { Student, Parent, Mentor, Faq };
+export { Student, Parent, Mentor, Faq, ChatLog, Badge, BadgeMeta, Reminder };
